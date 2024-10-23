f385e760e52ca28797308cbdc9365e43f598418f
Nicolas Casajus <rdev.nc@gmail.com>
2024-10-23 08:00:03 +0200

    doc: document function

--- R/get_cited_references.R
+++ R/get_cited_references.R
@@ -1,3 +1,35 @@
+#' Get cited references of one single article
+#'
+#' @description
+#' This function requests the OpenAlex database by using the package 
+#' `openalexR` and the function `oa_fetch()`. From a single DOI, it retrieves
+#' cited references of the original work.
+#'
+#' @param doi a `character` of length 1. The DOI of the paper for which cited 
+#'   references must be extracted.
+#'
+#' @return A `data.frame` with the following columns:
+#'   - `work_doi`: the DOI provided by the user, i.e. the original paper
+#'   - `work_oa_id`: the OpenAlex identifier of the original paper
+#'   - `n_cited_works`: the total number of cited references (indexed in OA)
+#'   - `cited_work_oa_id`: the OpenAlex identifier of the cited reference
+#'   - `cited_work_year`: the publication year of the cited reference
+#'   - `cited_work_source`: the source of the cited reference
+#'   - `cited_work_doi`: the DOI of the cited reference
+#' 
+#' @note
+#' Before using this function, ensure that you ran: 
+#' `options(openalexR.mailto = 'your_email')`.
+#' 
+#' @export
+#'
+#' @examples
+#' ## Be polite ----
+#' options(openalexR.mailto = 'rdev.nc@gmail.com')
+#' 
+#' ## Get cited references for one work (article) ----
+#' get_cited_references(doi = "10.1371/journal.pbio.3001640")
+
 get_cited_references <- function(doi) {
   
   ## Check argument ----
@@ -26,8 +58,9 @@ get_cited_references <- function(doi) {
   ## Prepare output ----
   
   cited_works <- data.frame("work_doi"      = doi,
-                            "work_oaid"     = NA,
+                            "work_oa_id"    = NA,
                             "n_cited_works" = NA)
+  
   
   ## Retrieve work metadata from DOI ----
   
@@ -35,16 +68,16 @@ get_cited_references <- function(doi) {
     as.data.frame()
   
   if (nrow(metadata) > 1) {
-    stop("The doi '", doi, "' returns multiple works in OpenAlex database")
+    stop("The DOI '", doi, "' returns multiple works in OpenAlex database")
   }
   
   
   ## Count cited works and extract OA ID ----
   
   if (nrow(metadata) == 1) {
-    cited_works$"work_oaid"     <- unlist(metadata$"id")
+    cited_works$"work_oa_id"    <- unlist(metadata$"id")
     cited_works$"n_cited_works" <- unlist(lapply(metadata$"referenced_works", 
-                                                  length))
+                                                 length))
   }
 
   
@@ -62,8 +95,9 @@ get_cited_references <- function(doi) {
   ## Extract columns ----
   
   references <- references[ , c("id", "publication_year", "so", "doi")]
-  colnames(references) <- c("cited_work_oaid", "cited_work_year", 
-                            "cited_work_journal", "cited_work_doi")
+  
+  colnames(references) <- c("cited_work_oa_id", "cited_work_year", 
+                            "cited_work_source", "cited_work_doi")
   
   references$"work_doi" <- doi
   
@@ -71,6 +105,9 @@ get_cited_references <- function(doi) {
   ## Clean output ----
   
   references <- merge(cited_works, references, by = "work_doi", all = TRUE)
+  references <- references[!duplicated(references$"cited_work_oa_id"), ]
+  
+  rownames(references) <- NULL
   
   references
 }
