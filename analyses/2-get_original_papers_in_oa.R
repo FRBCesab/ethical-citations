6b50f77090ae9ada00e76ff869a2f2d5299b9287
Nicolas Casajus <rdev.nc@gmail.com>
2024-10-25 09:26:56 +0200

    add code skeleton for oa

---
+++ analyses/2-get_original_papers_in_oa.R
@@ -0,0 +1,13 @@
+#' Retrieve articles for DAFNEE Journals and for 2023
+#' 
+
+
+## Import DAFNEE journals ----
+
+
+## Remove DAFNEE journals absent in OA ----
+
+
+## Get articles for 2023 ----
+
+# oa_get_original_papers()
