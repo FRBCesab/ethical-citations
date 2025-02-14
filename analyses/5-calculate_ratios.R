5224cf1cf4e236c3971357f66d1e5b15bef614f8
MiBe <beck.mibe@gmail.com>
2025-02-14 09:19:59 +0100

    calculate ratio at journal level

--- analyses/5-calculate_ratios.R
+++ analyses/5-calculate_ratios.R
@@ -1,18 +1,16 @@
 # For each article ('oa_work_id') calculate:
 #- number of refs not in dafnee (i.e. giving 'NA')
 #- # total references
-#- # for-profit (fp) and non-profit (np) journals
-#- proportion of fp (np) journals considering those that are in dafnee ('prop_np', 'prop_fp')
-#- ratio np/fp (not even sure if that makes sense???)
+#- # count for-profit (fp) and non-profit (np) journals
+#- proportion of fp (np) journals considering those that are in dafnee and identified as either np/fp ('prop_np_found', 'prop_fp_found') and overall ('prop_np', 'prop_fp)
 
-#For each journal ('res_journallvl'):
+#For each journal:
 #- mean & sd prop_np and prop_fp
 #- # articles
 
-
 #To Do:
-#- left_join df + dafnee gives (sometimes...) many-to-many warning -> check!
-#- properly save article-level tables (here as res_list)
+#- additional filtering of articles (at least 80% of refs found in crossref)
+#  and journals (based on quantile, exclude journals publishing extreme numbers of articles)
 
 library(dplyr)
 
@@ -32,12 +30,12 @@ dafnee<- dafnee %>% mutate(publisher_type=case_when(publisher_type=='For-profit'
 
 
 res_list<- list() #to save table per journal
-res_journallvl<-matrix(nrow=0, ncol = 12) #to save journal-level ratios
-colnames(res_journallvl)<- c('oa_source_id', 'n_articles', 'mean_prop_np', 
-                             'mean_prop_fp','mean_prop_nadafnee', 
-                             'mean_prop_np_found', 'mean_prop_fp_found',
-                             'sd_prop_np', 'sd_prop_fp', 'sd_prop_nadafnee',
-                             'sd_prop_np_found', 'sd_prop_fp_found')
+#res_journallvl<-matrix(nrow=0, ncol = 12) #to save journal-level ratios
+#colnames(res_journallvl)<- c('oa_source_id', 'n_articles', 'mean_prop_np', 
+#                             'mean_prop_fp','mean_prop_nadafnee', 
+#                             'mean_prop_np_found', 'mean_prop_fp_found',
+#                             'sd_prop_np', 'sd_prop_fp', 'sd_prop_nadafnee',
+#                             'sd_prop_np_found', 'sd_prop_fp_found')
 
 
 for (i in 1:length(list_refsfiles)) { #1:length(list_refsfiles)
@@ -92,97 +90,89 @@ for (i in 1:length(list_refsfiles)) { #1:length(list_refsfiles)
   res<- res[, c('oa_source_id', 'oa_work_id', 'n_refs', 'na_dafnee', 'np', 'fp',
                 'prop_np', 'prop_fp', 'prop_np_found', 'prop_fp_found', 
                 'prop_nadafnee')]
-
-  #calculate JOURNAL mean ratios
-  j_prop_np<- round(mean(res$prop_np[is.finite(res$prop_np)]), 4)
-  j_prop_fp<- round(mean(res$prop_fp[is.finite(res$prop_fp)]), 4)
-  j_prop_nadafnee<- round(mean(res$prop_nadafnee[is.finite(res$prop_nadafnee)]), 4)
-  
-  j_prop_np_found <- round(mean(res$prop_np_found[is.finite(res$prop_np_found)]), 4)
-  j_prop_fp_found <- round(mean(res$prop_fp_found[is.finite(res$prop_fp_found)]), 4)
-  
-  j_prop_np_sd<- round(sd(res$prop_np[is.finite(res$prop_np)]), 4)
-  j_prop_fp_sd<- round(sd(res$prop_fp[is.finite(res$prop_fp)]), 4)
-  j_prop_nadafnee_sd<- round(sd(res$prop_nadafnee[is.finite(res$prop_nadafnee)]), 4)
-  
-  j_prop_np_sd_found <- round(sd(res$prop_np_found[is.finite(res$prop_np_found)]), 4)
-  j_prop_fp_sd_found <- round(sd(res$prop_fp_found[is.finite(res$prop_fp_found)]), 4)
-  
-  
-  n_articles<- nrow(res)
   
   #bind results
   res_list<- append(res_list, list(res))
+  
+  #calculate JOURNAL mean ratios
+  #j_prop_np<- round(mean(res$prop_np[is.finite(res$prop_np)]), 4)
+  #j_prop_fp<- round(mean(res$prop_fp[is.finite(res$prop_fp)]), 4)
+  #j_prop_nadafnee<- round(mean(res$prop_nadafnee[is.finite(res$prop_nadafnee)]), 4)
+  #
+  #j_prop_np_found <- round(mean(res$prop_np_found[is.finite(res$prop_np_found)]), 4)
+  #j_prop_fp_found <- round(mean(res$prop_fp_found[is.finite(res$prop_fp_found)]), 4)
+  #  
+  #j_prop_np_sd<- round(sd(res$prop_np[is.finite(res$prop_np)]), 4)
+  #j_prop_fp_sd<- round(sd(res$prop_fp[is.finite(res$prop_fp)]), 4)
+  #j_prop_nadafnee_sd<- round(sd(res$prop_nadafnee[is.finite(res$prop_nadafnee)]), 4)
+  #
+  #j_prop_np_sd_found <- round(sd(res$prop_np_found[is.finite(res$prop_np_found)]), 4)
+  #j_prop_fp_sd_found <- round(sd(res$prop_fp_found[is.finite(res$prop_fp_found)]), 4)
+  
+  #n_articles<- nrow(res)
 
-  fd <- data.frame(
-    'oa_source_id' = df$oa_source_id[1],
-    'n_articles' = n_articles,
-    'mean_prop_np' = j_prop_np,  
-    'mean_prop_fp' = j_prop_fp,
-    'mean_prop_nadafnee' = j_prop_nadafnee ,  
-    'mean_prop_np_found' = j_prop_np_found,
-    'mean_prop_fp_found' = j_prop_fp_found,
-    'sd_prop_np' = j_prop_np_sd,
-    'sd_prop_fp' = j_prop_fp_sd,
-    'sd_prop_nadafnee' = j_prop_nadafnee_sd,
-    'sd_prop_np_found' = j_prop_np_sd_found,
-    'sd_prop_fp_found' = j_prop_fp_sd_found
-  )
+  #fd <- data.frame(
+  #  'oa_source_id' = df$oa_source_id[1],
+  #  'n_articles' = n_articles,
+  #  'mean_prop_np' = j_prop_np,  
+  #  'mean_prop_fp' = j_prop_fp,
+  #  'mean_prop_nadafnee' = j_prop_nadafnee ,  
+  #  'mean_prop_np_found' = j_prop_np_found,
+  #  'mean_prop_fp_found' = j_prop_fp_found,
+  #  'sd_prop_np' = j_prop_np_sd,
+  #  'sd_prop_fp' = j_prop_fp_sd,
+  #  'sd_prop_nadafnee' = j_prop_nadafnee_sd,
+  #  'sd_prop_np_found' = j_prop_np_sd_found,
+  #  'sd_prop_fp_found' = j_prop_fp_sd_found
+  #)
 
-  res_journallvl <- rbind(res_journallvl, fd)
+  #res_journallvl <- rbind(res_journallvl, fd)
   print(i)
 }
 
-
 #save
-saveRDS(res_list, file='outputs/ratios_articlelevel_unfilteredraw.rds')
-write.csv(res_journallvl, file='outputs/ratios_journallevel_unfilteredraw.csv')
-
-
-x <- readRDS('outputs/ratios_articlelevel_unfilteredraw.rds')
-x <- do.call(rbind.data.frame, x)
-
-n_ref_min <- quantile(na.omit(x$n_refs), 0.2) ##########
-
-x <- x[x$n_refs > n_ref_min, ]
+saveRDS(res_list, file='outputs/ratios_articlelevel_unfiltered.rds')
+#write.csv(res_journallvl, file='outputs/ratios_journallevel_unfiltered.csv')
 
 
 ## Aggregate by journal ----
 
-xxx <- x |> 
-  group_by(oa_source_id) |> 
-  summarise(
-    mean_prop_np  = mean("prop_np", na.rm = TRUE),
-    prop_fp       = mean("prop_fp", na.rm = TRUE),
-    prop_nadafnee = mean("prop_nadafnee", na.rm = TRUE),
-    prop_np_found = mean("prop_np_found", na.rm = TRUE),
-    prop_fp_found = mean("prop_fp_found", na.rm = TRUE)) |> 
+x <- readRDS('outputs/ratios_articlelevel_unfiltered.rds')
+x <- data.frame(do.call(rbind.data.frame, x))
+
+#remove articles citing < 5 refs
+n_ref_min <- 5 #loosing n=7433
+x <- x[x$n_refs > n_ref_min, ]
+
+#include only articles with at least 80% of number of citations as in crossdef
+#TO DISCUSS
+
+
+xx <- x |> 
+  group_by(oa_source_id) |>
+  summarise(across(c(prop_np:prop_nadafnee), list(mean=mean, sd=sd), na.rm=TRUE)) |> 
+  mutate(across(where(is.numeric), round, 4)) |>
   ungroup()
-
-
-
-##### STOP
-
-
+  
 #add journal name & dafnee status
-res_journallvl<- data.frame(res_journallvl)
-res_journallvl<- left_join(res_journallvl, dafnee, by='oa_source_id')
-write.csv(res_journallvl, file='outputs/ratios_journallevel.csv')
+xx<- left_join(xx, dafnee, by='oa_source_id')
+write.csv(xx, file='outputs/ratios_journallevel_unfiltered.csv', row.names=F)
 
+
+
+
+## Aggregate by category level  ----
+xx<- read.csv('outputs/ratios_journallevel_filtered_min5ref.csv')
 
 #quick view: results on publisher_type_level
-sum(is.na(res_journallvl$publisher_type)) #n=15 
+sum(is.na(xx$publisher_type)) #n=15 
 
-res_journallvl %>% 
-  group_by(publisher_type) %>% 
-  summarise(
-    mean_propnp_found = mean(as.numeric(mean_prop_np_found), na.rm = TRUE), 
-    mean_propfp_found = mean(as.numeric(mean_prop_fp_found), na.rm = TRUE)
-  )
+#merge 'university press' into 'non-profit' --> DISCUSS/DECIDE
+# on first look it was same direction of ratios as non-profit (np > fp)
+xx$publisher_type[xx$publisher_type=='University Press'] <- 'Non-profit'
 
-res_journallvl %>% 
-  group_by(publisher_type) %>% 
-  summarise(
-    mean_propnp = mean(as.numeric(mean_prop_np), na.rm = TRUE), 
-    mean_propfp = mean(as.numeric(mean_prop_fp), na.rm = TRUE)
-  )
+xx |> group_by(publisher_type) |>
+  summarise(across(contains('mean'), mean, na.rm=TRUE)) |> 
+  mutate(across(where(is.numeric), round, 4)) |>
+  ungroup()
+
