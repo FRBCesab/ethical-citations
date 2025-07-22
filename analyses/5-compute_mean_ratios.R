82a0fd5716047baa5d4b25674dc3984eb4cb7d85
Miriam B <107703216+Sikaiana@users.noreply.github.com>
2025-07-22 11:33:48 +0200

    violinboxplot, stats, simple counts (#13)

--- analyses/5-compute_mean_ratios.R
+++ analyses/5-compute_mean_ratios.R
@@ -42,12 +42,12 @@ if (length(pos) > 0) {
 
 ## Remove original papers w/ no citations (found in Dafnee) ----
 
-citations <- citations[citations$"n_refs" > citations$"na_dafnee", ]
+citations <- citations[citations$"n_refs" > citations$"na_dafnee", ] #losing: 19470 refs
 
 
 ## Remove original papers w/ less than 5 citations (found in Dafnee) ----
 
-citations <- citations[(citations$"n_refs" - citations$"na_dafnee_na") > 5, ]
+citations <- citations[(citations$"n_refs" - citations$"na_dafnee_na") > 5, ] #losing: 40326 refs
 
 citations <- as.data.frame(citations)
 
@@ -69,31 +69,31 @@ get_ratios <- function(data, business_model, is_academic) {
       data$"publisher_type" == business_model & data$"is_dafnee" == is_academic
     ),
   ]
-
+  
   total <- data$fp_nonacademic + data$fp_academic + data$np
-
+  
   data$fp_nonacademic <- data$fp_nonacademic / total
   data$fp_academic <- data$fp_academic / total
   data$np <- data$np / total
-
+  
   fp_nonacademic <- tapply(
     data$"fp_nonacademic",
     INDEX = data$"oa_source_id",
     mean
   )
-
+  
   fp_academic <- tapply(
     data$"fp_academic",
     INDEX = data$"oa_source_id",
     mean
   )
-
+  
   np <- tapply(
     data$"np",
     INDEX = data$"oa_source_id",
     mean
   )
-
+  
   data <- data.frame(
     "oa_source_id" = names(fp_nonacademic),
     business_model = business_model,
@@ -102,9 +102,9 @@ get_ratios <- function(data, business_model, is_academic) {
     fp_academic,
     np
   )
-
+  
   rownames(data) <- NULL
-
+  
   data
 }
 
@@ -129,17 +129,120 @@ ratios <- data.frame(
     apply(res3[, -c(1:3)], 2, mean)
   )
 )
-
 ratios$"business_model" <- c("NP", "FP", "FP")
 ratios$"is_academic" <- c(TRUE, TRUE, FALSE)
 ratios$"n_journals" <- c(nrow(res1), nrow(res2), nrow(res3))
+ratios$var <- 'mean ratio'
 
+SDs <- data.frame(
+  rbind(
+    apply(res1[, -c(1:3)], 2, sd),
+    apply(res2[, -c(1:3)], 2, sd),
+    apply(res3[, -c(1:3)], 2, sd)
+  )
+)
+SDs$"business_model" <- c("NP", "FP", "FP")
+SDs$"is_academic" <- c(TRUE, TRUE, FALSE)
+SDs$"n_journals" <- c(nrow(res1), nrow(res2), nrow(res3))
+SDs$var<- 'sd'
+
+all<- rbind(ratios, SDs)
 
 write.csv(
   x = ratios,
   file = here::here("outputs", "mean_ratio_per_publisher_type.csv"),
   row.names = FALSE
 )
+
+#plot journal level
+library(EnvStats)
+p<-ratio_per_journals %>% 
+  pivot_longer(fp_nonacademic:np, names_to='cat', values_to='value') %>%
+  mutate(journal_cat = paste(business_model, is_academic, sep='_'),
+         journal_cat = recode(journal_cat, 'FP_FALSE' = 'FP non-academic', 
+                              'FP_TRUE' = 'FP academic',
+                              'NP_TRUE' = 'NP'),
+         journal_cat = factor(journal_cat, levels=c('FP non-academic', 'FP academic', 'NP')),
+         cat = factor(cat, levels=c('fp_nonacademic', 'fp_academic', 'np')),
+         cat = recode(cat, 'fp_nonacademic' = 'FP non-academic',
+                      'fp_academic' = 'FP academic',
+                      'np' = 'NP')) %>%
+  filter(cat == 'NP') %>% #add for single type plot
+  ggplot(aes(x=journal_cat, y=value)) + #, fill=journal_cat
+  #geom_violin(fill='lightgray', color = NA) +
+  geom_boxplot(alpha=.6, width= .48, outlier.shape=21, outlier.alpha = 0, fill='#dddddd') +
+  ggdist::stat_halfeye(adjust= .5,
+                       width= .6,
+                       .width= 0,
+                       alpha= 1, point_colour=NA,
+                       fill='#888888'
+  ) +
+  #facet_wrap(~cat) + #remove for single type plot
+  #scale_fill_manual(values=c('#745392', '#909278', '#b0c700')) +#'#898584'  # '#c3c4b5' '#909278'
+  #scale_fill_manual(values=c('#777777', '#777777', '#777777')) +#for single type plot
+  xlab('Publisher type') + ylab('Citation of NP refs (%)') +
+  ylim(0,1) +
+  theme_bw() +
+  stat_n_text(size=3) +
+  theme(legend.position='none', #'none' for single type plot
+        panel.grid.major.y=element_blank(), 
+        panel.border=element_blank(),
+        #axis.text.x=element_blank(), #remove for single type plot
+        axis.line.x=element_line(), axis.line.y=element_line()) #+
+p
+ggsave(p, file='figures/final_journallevel_violinbox_all.png', dpi=200, width=6, height=3.5)
+ggsave(p, file='figures/final_journallevel_violinbox_np.png', dpi=200, width=3.5, height=3.5)
+
+
+#ANOVA/Kruskal-Wallis
+ratio_per_journals$journal_cat<- paste(ratio_per_journals$business_model, ratio_per_journals$is_academic, sep='_')
+kruskal.test( ~ journal_cat, data = ratio_per_journals)
+pairwise.wilcox.test(ratio_per_journals$np, ratio_per_journals$journal_cat,
+                     p.adjust.method = "bonferroni") #same result for 'BH'
+
+#sig: NP citations between all three journal types
+#sig: FP-academic citations between FP_academic-FP_nonacademic, FP_academic-NP
+#sig: FP-nonacademic citations between FP_nonacademic-FP_academic, FP_nonacademic-NP
+
+#final counts for MM/Sup (can be deleted in the end)
+#(to be run before each step of filtering)
+citations %>% mutate(journal_cat = paste(publisher_type, is_dafnee, sep='_')) %>%
+  mutate(nref_included=n_refs-na_dafnee_na) %>%
+  #group_by(journal_cat) %>%
+  summarise(n_articles=n(),
+            n_refs=sum(nref_included))
+
+n_distinct(citations$oa_source_name)
+
+#number of articles per journal
+citations %>% mutate(journal_cat = paste(publisher_type, is_dafnee, sep='_')) %>%
+  mutate(nref_included=n_refs-na_dafnee_na) %>%
+  group_by(oa_source_name) %>%
+  summarise(n_articles=n(),
+            n_refs=sum(nref_included)) %>% ungroup() %>%
+  summarise(mean_articles=mean(n_articles),
+            med_articles=median(n_articles),
+            sd_articles=sd(n_articles))
+
+#number of refs per article
+citations %>% mutate(journal_cat = paste(publisher_type, is_dafnee, sep='_')) %>%
+  mutate(nref_included=n_refs-na_dafnee_na) %>%
+  summarise(mean_refs=mean(nref_included),
+            med_refs=median(nref_included),
+            sd_refs=sd(nref_included))
+
+
+#plot mean/sd
+ratios %>% 
+  pivot_longer(fp_nonacademic:np, names_to='cat', values_to='value') %>%
+  mutate(journal_cat = paste(business_model, is_academic, sep='_'),
+         cat= factor(cat, levels=c('fp_nonacademic', 'fp_academic', 'np'))) %>%
+  ggplot(., aes(x=journal_cat, y=value)) +
+  geom_point() +
+  facet_wrap(~cat) +
+  theme_bw()
+
+
 
 
 # ratio_per_journals <- rbind(
